93d8961fef624ead7697e3502c63a0ef6da2eda8
Bigboss619$ <eugochukwu910@gmail.com>
2026-03-01 22:36:34 +0100

    updated

--- src/middlewares/error.midlleware.js
+++ src/middlewares/error.midlleware.js
@@ -1,5 +1,5 @@
 export default function errorMiddleware(err, req, res, next) {
-  console.error(err);
+  console.error(err.stack);
   res.status(500).json({
     message: err.message || "Internal Server Error"
   });
